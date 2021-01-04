56306abb4d60cf2ea319a3e03e8b3881e4c32a97
Amit Dubey <amitdubey8888@gmail.com>
2021-01-04 13:29:44 +0530

    new changes in database connection

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-An awesome app for online exam.
+Export: mongoexport --uri mongodb+srv://my-exam:my-exam@my-exam.3gbln.mongodb.net/my-exam --collection User --type JSON --out User.json
+Import: mongoimport --uri mongodb+srv://moshikhayaka:moshikhayaka@moshikhayaka.hnpcr.mongodb.net/moshikhayaka --collection User --type JSON --file User.json
--- app/config.js
+++ app/config.js
@@ -9,8 +9,8 @@ const S3 = new AWS.S3({
 
 module.exports = {
   S3: S3,
-  // database: 'mongodb://moshikhyaka:moshikhyaka@13.233.37.240:27017/moshikhyaka',
-  database: 'mongodb+srv://my-exam:my-exam@my-exam.3gbln.mongodb.net/my-exam?retryWrites=true&w=majority',
+  // database: 'mongodb://moshikhayaka:moshikhayaka@localhost:27017/moshikhayaka',
+  database: 'mongodb+srv://moshikhayaka:moshikhayaka@moshikhayaka.hnpcr.mongodb.net/moshikhayaka?retryWrites=true&w=majority',
   serverSecret: '$2y$12$4A2phQdjCl9fFvcuTHpn3.l7mZTBNagz4HrApfnY1tJYcyHgalifK',
   tokenExpire: '30d',
   google_api_key: 'AIzaSyCJnen-AB1eM6aoGKKLSnA1cyDIQknzD5Q',
