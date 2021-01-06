2330b87dcb43c55c80b8ef2a4b06d9f37115c8c7
Amit Dubey <amitdubey8888@gmail.com>
2021-01-06 12:09:47 +0530

    new changes in project

--- app/config.js
+++ app/config.js
@@ -1,17 +1,4 @@
-const AWS = require("aws-sdk");
-AWS.config.update({
-  region: "ap-south-1"
-});
-const S3 = new AWS.S3({
-  accessKeyId: 'AKIATWF3VOVMERWWLQT4',
-  secretAccessKey: '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',
-});
-
 module.exports = {
-  S3: S3,
   // database: 'mongodb://moshikhayaka:moshikhayaka@localhost:27017/moshikhayaka',
   database: 'mongodb+srv://moshikhayaka:moshikhayaka@moshikhayaka.hnpcr.mongodb.net/moshikhayaka?retryWrites=true&w=majority',
-  serverSecret: '$2y$12$4A2phQdjCl9fFvcuTHpn3.l7mZTBNagz4HrApfnY1tJYcyHgalifK',
-  tokenExpire: '30d',
-  google_api_key: 'AIzaSyCJnen-AB1eM6aoGKKLSnA1cyDIQknzD5Q',
 };
--- server.js
+++ server.js
@@ -1,28 +1,9 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const config = require('./app/config');
 const chatController = require('./app/controllers/chat');
-const apiRoutes = require('./app/routes/routes');
-const port = process.env.PORT || 6000;
-const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: true
-}));
-app.options('*', cors());
-app.use(bodyParser.urlencoded({
-  limit: '50mb',
-  extended: true
-}));
-app.use(bodyParser.json({
-  limit: '50mb'
-}));
 const options = {
   useNewUrlParser: true,
   useFindAndModify: false,
@@ -38,19 +19,6 @@ const options = {
 
 mongoose.connect(config.database, options);
 mongoose.set('useFindAndModify', false);
-app.set('superSecret', config.serverSecret);
-
-app.use('/api', apiRoutes);
-
-app.get('/', function(req, res) {
-  res.status(200);
-  res.json({
-    success: true,
-    message: 'Welcome to the coolest API on the earth!',
-  });
-});
-
-app.listen(port);
 
 // Web Socket Connection Start
 const WebSocket = require('ws');
